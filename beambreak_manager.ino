b2ffaef34a88d0f51007a24382dc228f2bd66661
Daniel Opara <58013370+bimmui@users.noreply.github.com>
2023-04-16 21:18:14 -0400

    Update beambreak_manager.ino with proper data structs for data and cleaner code
    Used a queue (circular buffer under the hood) for storing the sensor pins and cleaned up to code to look prettier. Also defined a struct to represent each of the four little boxes that will hold a pair of beambreak sensors. The way the program loop works should be fine when adding more beam compartments in the mix
    
    TODO:
    - changed the value of DISTANCE to what it actually is
    - implement some means of tracking which electromagnet to turn on, will probably be similar to the beam compartment struct that I've got going on

--- beambreak_manager.ino
+++ beambreak_manager.ino
@@ -1,22 +1,79 @@
-#define LEDPIN 13
-  // Pin 13: Arduino has an LED connected on pin 13
+/* 
+  IR Breakbeam sensor demo!
+*/
 
+
+#include "Queue.h"
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//   Defining digital pins and constants
+// - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+#define LEDPIN 13 // Pin 13: Arduino has an LED connected on pin 13
 #define SENSORPIN1 4
 #define SENSORPIN2 5
+const int DISTANCE = 33;
 
-// variables will change:
-int sensorState = 0, lastState=0;         // variable for reading the pushbutton status
-int sensorState2 = 0, lastState2=0;         // variable for reading the pushbutton status
 
-unsigned long firstsense = 0;
-unsigned long secondsense = 0;
-unsigned long currentTime = 0;
 
-float distance = 33;
-unsigned long velocity = 0;
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//   Necessary data structures
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+typedef struct doubleBeamCompartment {
+   int sensorpin1;
+   int sensorpin2;
+   
+   // defines the first set of sensors the ball encounters
+   int sensorstate1;
+   int laststate1;
+
+  // defines the second set of sensors the ball encounters
+   int sensorstate2;
+   int laststate2;
+} doubleBeamCompartment;
+
+Queue<doubleBeamCompartment> sensor_queue(4);
+
+
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//   Variables that will change throughout the program's lifetime
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+int curr_sensor1; // entering beam compartment
+int curr_sensorState1 = 0, curr_lastState1 = 0;
+
+int curr_sensor2; // exiting beam compartment
+int curr_sensorState2 = 0, curr_lastState2 = 0;
+   
+unsigned long first_timestamp = 0, second_timestamp = 0;
+unsigned long time_interval = 0;
+float velocity = 0;
+bool is_velocity_calculated = false;
+
+
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//   Helper functions
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+/*
+calculateVelocity
+Parameters: The recorded timestamps for each sensor in a compartment
+Purpose: Calculates velocity of the ball
+Returns: Nothing
+Effects: Modifies the value stored insider the "velocity" variable
+*/
+bool calculateVelocity(unsigned long t1, unsigned long t2) {
+  time_interval = t2 - t1;
+  velocity = DISTANCE / (float) time_interval;
+  return true;
+}
 
 
 void setup() {
+  Serial.begin(9600);
+
+  Serial.print("Setting up pin connections...");
   // initialize the LED pin as an output:
   pinMode(LEDPIN, OUTPUT);      
   // initialize the sensor pin as an input:
@@ -25,18 +82,29 @@ void setup() {
   
   pinMode(SENSORPIN2, INPUT);     
   digitalWrite(SENSORPIN2, HIGH); // turn on the pullup
+  Serial.println("Complete!");
+
+  Serial.print("Setting up sensor data structures...");
+
+  sensor_queue.push(doubleBeamCompartment{SENSORPIN1, SENSORPIN2, 0, 0, 0, 0});
+
+  doubleBeamCompartment temp = sensor_queue.pop();
+  curr_sensor1 = temp.sensorpin1;
+  curr_sensor2 = temp.sensorpin2;
+  Serial.println("Complete!");
+  Serial.println("Program set up complete! Beginning program...");
   
-  Serial.begin(9600);
+  
 }
 
 void loop(){
   // read the state of the pushbutton value:
-  sensorState = digitalRead(SENSORPIN1);
-  sensorState2 = digitalRead(SENSORPIN2);
+  curr_sensorState1 = digitalRead(curr_sensor1);
+  curr_sensorState2 = digitalRead(curr_sensor2);
 
   // check if the sensor beam is broken
   // if it is, the sensorState is LOW:
-  if (sensorState == LOW) {     
+  if (curr_sensorState1 == LOW) {     
     // turn LED on:
     digitalWrite(LEDPIN, HIGH);  
   } 
@@ -45,22 +113,17 @@ void loop(){
     digitalWrite(LEDPIN, LOW); 
   }
   
-  if (sensorState && !lastState) {
+  if (curr_sensorState1 && !curr_lastState1) {
     Serial.println("Unbroken");
   } 
-  if (!sensorState && lastState) {
+  if (!curr_sensorState1 && curr_lastState1) {
     Serial.println("Broken");
-    // firstsense = millis();
+    first_timestamp = millis();
   }
-  lastState = sensorState;
+  curr_lastState1 = curr_sensorState1;
   
-  
-  // read the state of the pushbutton value:
-  sensorState2 = digitalRead(SENSORPIN2);
 
-  // check if the sensor beam is broken
-  // if it is, the sensorState is LOW:
-  if (sensorState2 == LOW) {     
+  if (curr_sensorState2 == LOW) {     
     // turn LED on:
     digitalWrite(LEDPIN, HIGH);  
   } 
@@ -69,14 +132,24 @@ void loop(){
     digitalWrite(LEDPIN, LOW); 
   }
   
-  if (sensorState2 && !lastState2) {
+  if (curr_sensorState2 && !curr_lastState2) {
     Serial.println("Unbroken2");
   } 
-  if (!sensorState2 && lastState2) {
+  if (!curr_sensorState2 && curr_lastState2) {
     Serial.println("Broken2");
-    secondsense = millis();
-    currentTime = secondsense - firstsense;
-    velocity = distance / (float) currentTime
+    second_timestamp = millis();
+    is_velocity_calculated = calculateVelocity(first_timestamp, second_timestamp);
+    Serial.println("set to true");
   }
-  lastState2 = sensorState2;
+  curr_lastState2 = curr_sensorState2;
+
+  if (is_velocity_calculated == true) {
+    sensor_queue.push(doubleBeamCompartment{curr_sensor1, curr_sensor2, 0, 0, 0, 0});
+    doubleBeamCompartment temp = sensor_queue.pop();
+    curr_sensor1 = temp.sensorpin1;
+    curr_sensor2 = temp.sensorpin2;    
+    Serial.println("This is it: ");
+    Serial.println(velocity);
+    is_velocity_calculated = false;
+  }
 }
